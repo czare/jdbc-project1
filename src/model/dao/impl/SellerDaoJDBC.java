6c4d1779220a3999ca518dd9b5f5d2083d5a1008
czare <josue@gmx.at>
2019-07-03 23:21:57 -0300

    findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -56,13 +59,51 @@ public class SellerDaoJDBC implements SellerDao {
 				Seller seller = instantiateSeller(rs, department);
 				return seller;
 			}
+			return null;
 		}catch(SQLException ex) {
 			throw new DbException(ex.getMessage());
 		}finally {
 			DB.closeResultSet(rs);
 			DB.closeStatement(st);
 		}
+	}
+	
+	@Override
+	public List<Seller> findAll() {
+		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName FROM seller INNER JOIN department\n" + 
+					"ON seller.DepartmentId = department.Id WHERE DepartmentId = ? \n" + 
+					"ORDER BY Name"
+					);
+			st.setInt(1, department.getId());
+			rs = st.executeQuery();
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			while(rs.next()) {
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				if(dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				Seller seller = instantiateSeller(rs, dep);
+				list.add(seller);	
+			}
+			return list;
+		}catch(SQLException ex) {
+			throw new DbException(ex.getMessage());
+		}finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+		}
 	}
 
 	private Seller instantiateSeller(ResultSet rs, Department department) throws SQLException {
@@ -81,12 +122,6 @@ public class SellerDaoJDBC implements SellerDao {
 		department.setId(rs.getInt("DepartmentId"));
 		department.setName(rs.getString("DepName"));
 		return department;
-	}
-
-	@Override
-	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
